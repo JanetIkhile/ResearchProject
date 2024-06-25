f3fa1dd0af9df9ba4aa0faa5fc62ae50017f3893
Lincoln Gierisch <u1498729@umail.utah.edu>
2024-06-25 14:25:05 -0600

    Implemented longest pause and number of pauses

--- features-lincoln.js
+++ features-lincoln.js
@@ -1,9 +1,23 @@
+let results = null;
+let modal = document.getElementById('resultsModal');
+let modalContent = document.getElementById('modalBodyContent');
+let startTime = 0;
+let numberPauses = 0;
+let pauseIdentifier = 0;
+let pauseCounter = 0;
+let pauseDuration = 0;
+let specificPauseDuration = 0;
+let currentTime = 0;
+
+
 document.addEventListener("touchstart", e => {
     const touch = e.changedTouches[0];
     touchStartX = touch.pageX;
     touchStartY = touch.pageY;
 
     startTime = Date.now();
+    pauseIdentifier = Date.now();
+
 
     const pointer = document.createElement("div")
     pointer.classList.add("dot")
@@ -11,8 +25,11 @@ document.addEventListener("touchstart", e => {
     pointer.style.left = `${touchStartX}px`
     pointer.id = touch.identifier
 
+    pauseCounter = -1;
+    pauseDuration = 0;
+
     document.body.append(pointer);
-    console.log(touch);
+    // console.log(touch);
 
 });
 
@@ -20,13 +37,29 @@ document.addEventListener("touchmove", e => {
     const touch = e.changedTouches[0];
     let currentX = touch.pageX; //Store current x location as finger is moving
     let currentY = touch.pageY; //Store current y location as finger is moving
-    let currentTime = Date.now();
+    currentTime = Date.now();
+
+    //Number of pauses while the person is still touching the screen
+    //100 ms or longer
+    if (pauseIdentifier <= currentTime - 100) {
+        pauseCounter = pauseCounter + 1;
+        specificPauseDuration = currentTime - pauseIdentifier;
+        console.log(specificPauseDuration);
+        //If this specific pause is longer than the previous longest pause, the longest pause is updated
+        if (specificPauseDuration > pauseDuration) {
+            pauseDuration = specificPauseDuration;
+            // console.log(pauseDuration);
+
+        }
+    }
+
+    pauseIdentifier = Date.now();
 
     const pointer = document.getElementById(touch.identifier)
     pointer.style.top = `${touch.pageY}px`
     pointer.style.left = `${touch.pageX}px`
 
-    console.log(touch);
+    //console.log(touch);
 });
 
 document.addEventListener("touchend", e => {
@@ -37,7 +70,24 @@ document.addEventListener("touchend", e => {
     pointer.remove();
 
     endTime = Date.now();
-    end = (endTime - startTime)
-    console.log(end);
+    totalTime = (endTime - startTime)
+    console.log(totalTime);
 
+    if (pauseCounter < 0) {
+        pauseCounter = 0;
+    }
+
+    if (pauseCounter <= 0) {
+        pauseDuration = 0;
+    }
+
+    results = `Total Time: ${totalTime} ms
+    Total Pauses: ${pauseCounter} pauses
+    Longest Pause Duration: ${pauseDuration} ms`;
+
+    modalContent.innerText = results;
+    modal.style.display = 'block'
 });
+function closeModal() {
+    modal.style.display = 'none';
+}
