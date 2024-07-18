6e9bad18e9172bb1e4e4e24c67a22d18ab110aa0
JanetIkhile <u1471461@umail.utah.edu>
2024-07-18 12:30:28 -0600

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const bodyParser = require('body-parser');
 app.use(express.json())
 app.use(cors())
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../client/build')));
+//app.use(express.static(path.join(__dirname, '../client/build')));
 
 const PROD_URL = process.env.PROD_URL;
 const LOCAl_URL = process.env.LOCAL_URL;
@@ -22,10 +22,10 @@ console.log('PROD URL:', PROD_URL);
 //routes
 app.use(`${PROD_URL}/api/measures`, measuresRoutes);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-}
-);
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// }
+// );
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.MONGODB_URI)
